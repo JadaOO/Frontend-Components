b876bbc394cebf80c419e1acc143b7108533cd76
JadaOO <98721834+JadaOO@users.noreply.github.com>
2024-10-10 11:07:14 -0700

    grid

---
+++ src/Grid/Grid.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Grid: React.FC = () => {
+  return (
+    <div>
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+      <div>Item 4</div>
+      <div>Item 5</div>
+      <div>Item 6</div>
+    </div>
+  );
+};
+
+export default Grid;
