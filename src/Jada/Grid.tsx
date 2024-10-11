83450ae58ed28426b3c11d00c1765ffda5e9f36f
JadaOO <98721834+JadaOO@users.noreply.github.com>
2024-10-11 13:51:47 -0700

    done grid

--- src/Jada/Grid.tsx
+++ src/Jada/Grid.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Grid.css";
 
 // interface gridProps {
 //     rows
@@ -6,22 +7,53 @@ import React, { useState } from "react";
 
 const Grid: React.FC = () => {
   //use Array.from to create 3 new array grid
-  const grid = Array.from({ length: 3 }, () => new Array(3).fill("X"));
-  const [on, setOn] = useState(false);
+  // const grid = Array.from({ length: 3 }, () => new Array(3).fill("X"));
+  const initialGridState = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+  ];
+
+  const grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  const [gridState, setGridState] = useState(initialGridState);
+
+  const handleClick = (rowIndex: number, cellIndex: number) => {
+    const newGridState = gridState.map((row, currentRowIndex) =>
+      row.map((cell, currentCellIndex) =>
+        currentRowIndex === rowIndex && currentCellIndex === cellIndex
+          ? !cell
+          : cell
+      )
+    );
+    setGridState(newGridState);
+  };
+
+  // const [on, setOn] = useState(false);
 
   return (
-    <div>
+    <div className="grid-container">
       {grid.map((row, rowIndex) => (
-        <div key={rowIndex}>
+        <div key={rowIndex} className="grid-row">
           {row.map((cell, cellIndex) => (
             <div
               key={`${rowIndex}-${cellIndex}`}
-              className={`cell ${on ? "on" : "off"}`}
-              onClick={() => {
-                setOn(!on);
-              }}
+              className="grid-cell"
+              // onClick={() => {
+              //   setOn(!on);
+              // }}
             >
-              {cell}
+              <button
+                className={`grid-cell ${
+                  gridState[rowIndex][cellIndex] ? "on" : "off"
+                }`}
+                onClick={() => handleClick(rowIndex, cellIndex)}
+              >
+                {cell}
+              </button>
             </div>
           ))}
         </div>
